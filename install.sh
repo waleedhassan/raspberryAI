2470de39539de6a57f53e5a3e83e715e4f869037
Waleed Hassan <waleedhassan2000@gmail.com>
2026-04-20 22:04:57 +0300

    fix font

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ sudo apt-get install -y \
     libsdl2-2.0-0 libsdl2-image-2.0-0 libsdl2-ttf-2.0-0 libsdl2-mixer-2.0-0 \
     libfreetype6-dev libjpeg-dev \
     fonts-noto-core fonts-noto-cjk fonts-liberation \
-    fonts-hosny-amiri fonts-noto-naskh-arabic
+    fonts-hosny-amiri
 
 echo "==> python venv"
 python3 -m venv "$VENV"
